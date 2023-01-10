4ac64be1c36d5a9b56401eb6657a5994b63c2556
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-01-10 18:09:18 +0000

    add post /register

---
+++ apps/server/routes/register.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET login. */
+router.post('/', function(req, res, next) {
+  res.send('NOT IMPLEMENTED');
+});
+
+module.exports = router;
