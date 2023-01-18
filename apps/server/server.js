b0ad0d139da262f84e34c5cbb23aa7c48a116a50
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-01-18 16:13:18 +0000

    connection test to mongoDB

--- apps/server/server.js
+++ apps/server/server.js
@@ -4,3 +4,11 @@ const port = process.env.PORT || '3000'
 app.listen(port, () => {
     console.log(`PhoneFinder API is listening on port ${port}.`)
 })
+
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI)
+}
