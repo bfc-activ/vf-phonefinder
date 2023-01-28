5f64333a4004593fc620bb8ed88b9d1829326a59
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-01-28 19:54:06 +0000

    Create tag model

---
+++ apps/server/app/models/tag.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const TagSchema = new Schema({
+    name: { type: String, required: true }
+})
+
+module.exports = mongoose.model('tag', TagSchema)
