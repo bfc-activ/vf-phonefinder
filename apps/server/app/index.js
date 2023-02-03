b9318d32d4c2564f834d13e8b11c0935a859e2b8
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-03 22:46:28 +0000

    add error handling middleware
    The error will be taken and responded within a json body.

--- apps/server/app/index.js
+++ apps/server/app/index.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const logger = require("morgan");
-require("dotenv").config();
-const app = express();
+const express = require("express")
+const logger = require("morgan")
+const errorHandler = require('./helpers/errorHandler')
+
+require("dotenv").config()
+const app = express()
 
 module.exports = app
+    // middleware tools
   .use(logger("dev"))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
 
+    // routes
   .use("/", require("./routes/index"))
   .use("/users", require("./routes/user"))
   .use("/login", require("./routes/login"))
   .use("/register", require("./routes/register"))
-  .use("/questions", require("./routes/questions"));
+  .use("/questions", require("./routes/questions"))
+
+    // error handling
+    .use(errorHandler)
