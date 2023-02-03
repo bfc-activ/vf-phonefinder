dff072b5d6c7cf282dd01257b3318df776e87f97
Ingus Jansons <ingusjan@gmail.com>
2023-02-03 13:45:46 +0000

    fix: add dotenv

--- apps/server/app/index.js
+++ apps/server/app/index.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const logger = require('morgan')
-
-const app = express()
+const express = require("express");
+const logger = require("morgan");
+require("dotenv").config();
+const app = express();
 
 module.exports = app
-    .use(logger('dev'))
-    .use(express.json())
-    .use(express.urlencoded({ extended: false }))
+  .use(logger("dev"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
 
-    .use('/', require('./routes/index'))
-    .use('/users', require('./routes/user'))
-    .use('/login', require('./routes/login'))
-    .use('/register', require('./routes/register'))
-    .use('/questions', require('./routes/questions'))
-
+  .use("/", require("./routes/index"))
+  .use("/users", require("./routes/user"))
+  .use("/login", require("./routes/login"))
+  .use("/register", require("./routes/register"))
+  .use("/questions", require("./routes/questions"));
