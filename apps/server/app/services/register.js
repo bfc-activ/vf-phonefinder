f474c3e9a8ce08e28c7151bbe5ad54dd3ebeb77a
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-03 22:48:19 +0000

    add check existing user to register and start creating model

--- apps/server/app/services/register.js
+++ apps/server/app/services/register.js
@@ -1,20 +1,42 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const passport = require('passport')
 
-const UserSchema = require('../helpers/validation/user')
+const UserValidationSchema = require('../helpers/validation/user')
+const UserModel = require('../models/user')
 
 module.exports = {
     register: async () => {
         return "post register"
     },
     validateRequestBody: async (body) => {
-        try{
-            const value = await UserSchema.validateAsync(body)
-            console.log(value)
-        } catch (e) {
-            console.error(`Error validating body: ${e}`)
+        await UserValidationSchema.validateAsync(body)
+        // using Joi user schema to validate the request body.
+    },
+    registerUser: async (body) => {
+        // 1. Check the email doesn't already exist
+        if (await UserModel.findOne({ email: body.email })) {  // will be null if not found
+            const err = new Error(`A user with the email "${body.email}" already exists.`)
+            err.status = 400  // it's the user's fault.
+            throw err
         }
+
+        // 2. Hash the password
+        const hashedPassword = await bcrypt.hash(body.password, 10)
+
+        await UserModel.create({
+            email: body.email,
+            username: body.username,
+            password: hashedPassword,
+            isAdmin: body.isAdmin
+        })
+
+
+        // 3. JWT token baby.
+
     }
+}
+
+async function hashPassword(password) {
+    bcrypt.hash(password, 10)
 }
 
