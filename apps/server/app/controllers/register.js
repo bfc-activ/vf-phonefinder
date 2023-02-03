a4664f987637dd180197c1838e8c54772c5a7589
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-03 22:47:59 +0000

    Update register.js

--- apps/server/app/controllers/register.js
+++ apps/server/app/controllers/register.js
@@ -1,53 +1,22 @@
-const { register, validateRequestBody } = require('../services/register')
+const { validateRequestBody, registerUser } = require('../services/register')
 const jwt = require('jsonwebtoken')
 
 module.exports = {
     post: async (req, res, next) => {
-        await validateRequestBody(req.body)
-
-
-
-        // try {
-        //     // validateInput()
-        // } catch (e) {
-        //
-        // }
-        // const { error, isValid } = validateRegisterInput(req.body)
-
-        // try {
-        //     // get data from request
-        //     const user = req.user
-        //     const token = req.body.headers.Authorization
-        //     const tokenArray = token.split(' ')
-        //
-        //     jwt.verify(
-        //         tokenArray[1],
-        //         'secret',
-        //         { id: user.id },
-        //         function (err, decoded) {
-        //             if (err) {
-        //                 res.status.send({
-        //                     success: false,
-        //                     message: 'Token provided was manipulation'
-        //                 })
-        //             } else {
-        //                 next()
-        //             }
-        //         }
-        //     )
-        // } catch (e) {
-        //     console.log(e)
-        //     res.status(500).send({ success: false, error: e.message })
-        // }
+        // validate request body
+        try {
+            await validateRequestBody(req.body)
+        } catch (e) {
+            e.status = 400
+            return next(e)
+        }
 
         try {
-
-            res.send(await register())
-        } catch (err) {
-            console.error(`Error while posting register`, err.message)
-            next(err)
+            await registerUser(req.body)
+        } catch (e) {
+            return next(e)
         }
+
+        res.send('OK')
     }
 }
-
-
