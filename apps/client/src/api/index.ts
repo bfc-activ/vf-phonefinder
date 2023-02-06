49467226350070575d2d751726dac4c01b982dc1
Ingus Jansons <ingusjan@gmail.com>
2023-02-06 15:39:30 +0000

    fix: use prod api endpoint when in prod

--- apps/client/src/api/index.ts
+++ apps/client/src/api/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.PROD
+    ? "https://phonefinder-api-68hbi.ondigitalocean.app/"
+    : "http://localhost:3000/",
 });
 
 export default api;
