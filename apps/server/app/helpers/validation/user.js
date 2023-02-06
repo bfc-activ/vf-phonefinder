90f5ae2d9642874830b942da48958ef1fd56329c
Ingus Jansons <ingusjan@gmail.com>
2023-02-06 15:43:33 +0000

    fix: user validation allow spaces

--- apps/server/app/helpers/validation/user.js
+++ apps/server/app/helpers/validation/user.js
@@ -2,7 +2,11 @@ const Joi = require("joi");
 
 const userSchema = Joi.object({
   email: Joi.string().required(),
-  name: Joi.string().alphanum().min(3).max(50).required(),
+  name: Joi.string()
+    .min(3)
+    .max(50)
+    .required()
+    .regex(/^\w+(?:\s+\w+)*$/),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
 });
 
