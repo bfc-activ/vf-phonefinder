5c9e4f67d8559fd19e186ec96bebeafc9625d815
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-07 15:16:58 +0000

    Create populatePhones.js

---
+++ apps/server/app/config/populatePhones.js
@@ -0,0 +1,18 @@
+let phones = [
+    {
+        name: "iPhone 14",
+        price: 1230,
+        description: "some text"
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+console.log(phones)
