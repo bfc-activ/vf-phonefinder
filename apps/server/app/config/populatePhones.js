f7056e7e2c8e005ad1fdf98e89d1d5267b571068
teniona <87488643+teniona@users.noreply.github.com>
2023-02-08 09:57:14 +0000

    Update populatePhones.js

--- apps/server/app/config/populatePhones.js
+++ apps/server/app/config/populatePhones.js
@@ -1,8 +1,123 @@
 let phones = [
     {
-        name: "iPhone 14",
-        price: 1230,
-        description: "some text"
+        name: "Apple iPhone 14",
+        price: 811,
+        description: "Capture stunning photos in low light and bright light. Crash Detection, a new safety feature, calls for help when you can’t.",
+        operatingSystem:"iOS 16",
+        screenSize: 6.06,
+        is5GCompatible: True,
+        hasWirelessCharging: True,
+        storageCapacity: "128GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Apple_iPhone_14_blue-full-product-front-600.png"
+        
+    },
+    {
+        name: "Apple iPhone 13 Pro Max",
+        price: 893,
+        description: "Super Retina XDR display with ProMotion, a lightning-fast A15 Bionic chip and the best battery life ever in an iPhone.",
+        operatingSystem:"iOS 15",
+        screenSize: 6.7 ,
+        is5GCompatible: True ,
+        hasWirelessCharging: True,
+        storageCapacity: "256GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Apple_iPhone_13_Pro_Max_alpine_green-full-product-front-600.png"
+        
+    },
+    {
+        name: "Apple iPhone SE",
+        price: 271,
+        description: "A small iPhone with huge performance – the iPhone SE is the most powerful 4.7-inch iPhone ever.",
+        operatingSystem:"iOS 13",
+        screenSize: 4.7,
+        is5GCompatible: False,
+        hasWirelessCharging: True,
+        storageCapacity: "64GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Apple_iPhone_SE_product_red-full-product-front-600.png"
+        
+    },
+    {
+        name: "Samsung Galaxy S23 Ultra",
+        price: 1237,
+        description: "Nothing will hold you back with the S23 Ultra. Capture epic moments and enjoy uninterrupted gaming and streaming.",
+        operatingSystem:"Android 13",
+        screenSize: 6.8,
+        is5GCompatible: True,
+        hasWirelessCharging: True,
+        storageCapacity: "512GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Samsung_Galaxy_S23_Ultra_green-full-product-front-600.png"
+        
+    },
+    {
+        name: "Samsung Galaxy Z Flip4",
+        price: 965,
+        description: "Watch your favourite content on an AMOLED 2X 6.7-inch display. Snap selfies with Flex Cam and enjoy a long-lasting battery. ",
+        operatingSystem:"Android 12",
+        screenSize: 6.7,
+        is5GCompatible: True ,
+        hasWirelessCharging: True,
+        storageCapacity: "128GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Samsung_Galaxy_Z_Flip4_bora_purple-full-product-front-600.png"
+        
+    },
+    {
+        name: "Samsung Galaxy A13",
+        price: 189,
+        description: "Discover a triple lens camera system, crisp 90Hz Infinity-V HD+ display, 64GB internal memory and a long-lasting battery. ",
+        operatingSystem:"Android 12",
+        screenSize: 6.6,
+        is5GCompatible: False,
+        hasWirelessCharging: False,
+        storageCapacity: "64GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Samsung_Galaxy_A13_black-full-product-front-600.png"
+        
+    },
+    {
+        name: "Google Pixel 6a",
+        price: 225,
+        description: "Take beautiful shots with the 12-megapixel wide lens camera with Magic Eraser and stay productive with the adaptive battery. ",
+        operatingSystem:"Android 12",
+        screenSize: 6.1,
+        is5GCompatible: True,
+        hasWirelessCharging: False,
+        storageCapacity: "128GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Google_Pixel_6A_charcoal-full-product-front-600-v2.png"
+        
+    },
+    {
+        name: "Sony Xperia 10 IV",
+        price: 369,
+        description: "Enjoy a super lightweight design with 5,000mAh battery and capture special moments with the ultra-wide angle 16mm lens. ",
+        operatingSystem:"Android 12",
+        screenSize: 6,
+        is5GCompatible: True,
+        hasWirelessCharging: False ,
+        storageCapacity: "128GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Sony_Xperia_10_IV_black-full-product-front-600.png"
+        
+    },
+    {
+        name: "Xiaomi 12 Pro",
+        price: 1001,
+        description: "Discover 50MP wide-angle photography and intelligent 120W HyperCharge. Enjoy dynamic power with Snapdragon® 8 Gen 1 chipset.",
+        operatingSystem:"Andriod 12",
+        screenSize: 6.73,
+        is5GCompatible: True,
+        hasWirelessCharging: False,
+        storageCapacity: "256GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Xiaomi_12_Pro_gray-full-product-front-600.png"
+        
+    },
+    {
+        name: "OPPO A54s",
+        price: 117,
+        description: "The OPPO A54s boasts a stunning 6.5-inch HD+ Eye-care Display, plus a 50MP AI Triple Camera and a long-lasting battery.",
+        operatingSystem:"Android 11",
+        screenSize: 6.52,
+        is5GCompatible: False,
+        hasWirelessCharging: False,
+        storageCapacity: "128GB",
+        photoURL: "https://cdn.vodafone.co.uk/en/assets/images/desktop/Oppo_A54s_crystal_black-full-product-front-600.png"
+        
     },
     {
 
