786f31784353af1aaaec4acb340a39d3f0e618f5
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-10 23:20:54 +0000

    Update phone.js

--- apps/server/app/models/phone.js
+++ apps/server/app/models/phone.js
@@ -1,7 +1,45 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const PhoneSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    photoURL: {
+        type: Number,
+        required: true
+    },
+    operatingSystem: {
+        type: String,
+        required: true
+    },
+    screenSizeInch: {
+        type: Number,
+        required: true
+    },
+    is5GCompatible: {
+        type: Boolean,
+        required: true
+    },
+    hasWirelessCharging: {
+        type: Boolean,
+        required: true
+    },
+    storageCapacityGB: {
+        type: Number,
+        required: true
+    },
+    identifier_ids: {
+        type: String,
+        ref: 'Identifier'
+    }
 })
-
 module.exports = Phone = mongoose.model('Phone', PhoneSchema)
