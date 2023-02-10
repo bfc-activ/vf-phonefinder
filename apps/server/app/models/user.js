f08cdaad12e3e35df4aec3c0cce9e4d2a8d65269
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-10 23:21:05 +0000

    Update user.js

--- apps/server/app/models/user.js
+++ apps/server/app/models/user.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-
 const Schema = mongoose.Schema;
-
 const UserSchema = new Schema({
     name: {
         type: String,
@@ -25,5 +23,4 @@ const UserSchema = new Schema({
     timestamps: true,
   }
 );
-
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = User = mongoose.model("User", UserSchema);
