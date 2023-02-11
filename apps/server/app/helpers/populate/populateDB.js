83630e86ad784c02f5f4c05044dff947ad2f4cd0
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-11 00:22:35 +0000

    populate answers

---
+++ apps/server/app/helpers/populate/populateDB.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+// Future proofing setting
+mongoose.set('strictQuery', false)
+mongoose.set('strict', 'throw')  // throw errors when schema validation fails
+
+const connect = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI
+        await mongoose.connect(MONGO_URI)
+        console.log('Successful connection to MongoDB')
+    } catch (e) {
+        console.log('Please ensure that the MONGO_URI environment variable is set correctly. You may need to put parentheses around the URI string.')
+        throw e
+    }
+}
+
+const Question = require('../../models/question')
+const User = require('../../models/user')
+const Answer = require('../../models/answer')
+const answerData = require('../../models/sampleData/allAnswers.json')
+
+const populateAnswers = async () => {
+    await Answer.create(answerData.answers)
+    // for (const answer of answerData.answers) {
+    //     console.log(answer)
+    //     await Answer.create(answer)
+    // }
+}
+
+const populateQuestions = async () => {
+
+}
+
+const populateDB = async () => {
+    await populateAnswers()
+    await populateQuestions()
+    // await populatePhones()
+    // await populateUsers()
+}
+
+connect().then(() => {
+    populateDB().then(() => {
+        console.log('done')
+    })
+})
+
+
+
+module.exports = { populateDB }
