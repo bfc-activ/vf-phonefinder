1665da9d38fb46c14e8ddd4c40b44fc62dd938c8
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-14 23:02:39 +0000

    add note comment

--- apps/server/app/services/questions.js
+++ apps/server/app/services/questions.js
@@ -4,7 +4,14 @@ const Question = require('../models/question')
 module.exports = {
     get: async () => {
         const response = await Question.find().populate('answers')
-        console.log(response)
+        /**
+         * Note: This will return the array of documents in the correct order.
+         *       I think it is because the _id is a unique number. So, there
+         *       is no need to sort the result.
+         *
+         *       Additionally, populate answers embeds the answer object in the
+         *       answers array in the question with the `populate` method.
+         */
         return response
     }
 }
