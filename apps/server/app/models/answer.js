d9cb5f421a9834852d7df4c4f8a1a8248fea539c
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-14 22:50:40 +0000

    rename id to _id and require position

--- apps/server/app/models/answer.js
+++ apps/server/app/models/answer.js
@@ -1,24 +1,20 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const AnswerSchema = new Schema(
-    {
-        id: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        displayText: {
-            type: String
-        },
-        photoURL: {
-            type: String
-        },
-        position: {
-            type: Number,
-            required: true
-        }
+const AnswerSchema = new Schema({
+    _id: {
+        type: String
+    },
+    displayText: {
+        type: String
+    },
+    photoURL: {
+        type: String
+    },
+    position: {
+        type: Number,
+        required: true,
     }
-);
+});
 
 module.exports = Answer = mongoose.model("Answer", AnswerSchema);
