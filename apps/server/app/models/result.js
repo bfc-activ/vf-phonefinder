c2ea1ce54b9e1be11afb82008927d2191b4a37bc
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-15 10:21:31 +0000

    update result Schema to use new answers reference

--- apps/server/app/models/result.js
+++ apps/server/app/models/result.js
@@ -2,20 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const ResultSchema = new Schema({
-        user_id: {
-            type: String,
-            required: true,
-            ref: 'User'
-        },
-        answer_ids: [{
-            type: String,
-            required: true
-        }]
+    user_id: {
+        type: String,
+        required: true,
+        ref: 'User'
     },
-    {
-        // Adds createdAt and updatedAt fields in MongoDB
-        timestamps: true,
-    }
-);
+    answers: [{
+        type: String,
+        ref: 'Answer'
+    }],
+}, {
+    // Adds createdAt and updatedAt fields in MongoDB
+    timestamps: true,
+});
 
 module.exports = Result = mongoose.model("Result", ResultSchema);
