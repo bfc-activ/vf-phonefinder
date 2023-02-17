8004bd6114133feb61e0cf0926b33c35b7ef684b
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-17 13:16:16 +0000

    log full error

--- apps/server/app/middleware/errorHandler.js
+++ apps/server/app/middleware/errorHandler.js
@@ -1,5 +1,5 @@
 module.exports = (e, req, res, next) => {  // An error handler is defined by having at least 4 parameters
-    console.error(e.message)
+    console.error(e)
     res.status(e.status || 500)  // default error 500 (server-side)
     res.json({error: e.message})  // JSON for FE
 }
